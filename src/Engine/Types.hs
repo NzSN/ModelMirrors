37de107b9480f61145ee85ceb1425904e9151011
NzSN <nzsn0101@gmail.com>
2026-05-29 15:12:17 +0800

    feat: add Engine.Types module

---
+++ src/Engine/Types.hs
@@ -0,0 +1,24 @@
+module Engine.Types where
+
+import Apalache.Types (Value)
+import Data.Map.Strict (Map)
+import Data.Text (Text)
+
+data Step = Step
+  { stepIdx  :: !Int
+  , stepVars :: !(Map Text Value)
+  } deriving (Show, Eq)
+
+data VarDiff
+  = ValueMismatch !Text !Value !Value
+  | MissingVar    !Text !Value
+  | ExtraVar      !Text !Value
+  deriving (Show, Eq)
+
+data StateDiff
+  = StatesMatch
+  | StateMismatch
+      !(Map Text Value)
+      !(Map Text Value)
+      ![VarDiff]
+  deriving (Show, Eq)
