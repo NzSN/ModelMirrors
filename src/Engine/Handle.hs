e2dace7ead04d96aac9967092036b0fb6b716d5e
NzSN <nzsn0101@gmail.com>
2026-05-29 16:42:41 +0800

    feat: add Engine.Handle with EngineM typeclass

---
+++ src/Engine/Handle.hs
@@ -0,0 +1,19 @@
+module Engine.Handle (EngineM (..)) where
+
+import Apalache.Types (ItfTrace (..), Value)
+import Data.Map.Strict (Map)
+import Data.Text (Text)
+import Engine.Core (diffState, traceSteps)
+import Engine.Types (Step (..), StateDiff (..))
+
+class Monad m => EngineM m where
+  replayTrace :: ItfTrace -> (Step -> m (Map Text Value)) -> m [StateDiff]
+  replayTrace trace report = go (traceSteps trace)
+    where
+      go [] = pure []
+      go (step : steps) = do
+        actual <- report step
+        let diff = diffState (stepVars step) actual
+        case diff of
+          StatesMatch -> (diff :) <$> go steps
+          StateMismatch{} -> pure [diff]
